33000e1a9e67f03e2b0e891a5c5657cec62d483b
chriscoey <coey.chris@gmail.com>
2016-03-04 20:10:00 -0500

    added pajarito

--- gateSizing.jl
+++ gateSizing.jl
@@ -1,18 +1,17 @@
 
-using Convex, SCS
+using Convex, SCS, Pajarito, Gurobi, ECOS
 
 fe = [1, 0.8, 1, 0.7, 0.7, 0.5, 0.5] .* [1, 2, 1, 1.5, 1.5, 1, 2]
 Cout6 = 10
 Cout7 = 10
 
+xUB = 5 #40
+
+
 y = Variable(7)
+z = Variable(xUB,7,Positive(),:Bin)
 
-C = ones(7) + exp(y)
-A = sum(exp(y))
-P = sum(fe .* exp(y))
-R = exp(-y)
-
-D1 = exp(-y[1]) + 1
+D1 = exp(-y[1]) + exp(-y[1] + y[4])
 D2 = 2 * exp(-y[2]) + exp(-y[2] + y[4]) + exp(-y[2] + y[5])
 D3 = 2 * exp(-y[3]) + exp(-y[3] + y[5]) + exp(-y[3] + y[7])
 D4 = 2 * exp(-y[4]) + exp(-y[4] + y[6]) + exp(-y[4] + y[7])
@@ -22,10 +21,23 @@ D7 = Cout7 * exp(-y[7])
 
 problem = minimize(maximum([
     (D1+D4+D6), (D1+D4+D7), (D2+D4+D6), (D2+D4+D7),
-    (D2+D5+D7), (D3+D5+D6), (D3+D7)]
-    ), y >= 0, P <= 20, A <= 100)
+    (D2+D5+D7), (D3+D5+D6), (D3+D7)]),
+    y >= 0,
+    sum(fe .* exp(y)) <= 20,
+    sum(exp(y)) <= 100,
+    )
 
-solve!(problem, SCSSolver(verbose=0))
+for i=1:7
+    problem.constraints += sum(z[:,i]) == 1
+    problem.constraints += y[i] == sum([log(j) * z[j,i] for j=1:xUB])
+end
 
-println(problem.optval)
-println(y.value)
+#solve!(problem, SCSSolver(verbose=0))
+
+
+
+
+
+Convex.solve!(problem, PajaritoConicSolver(
+    mip_solver=GurobiSolver(),
+    conic_solver=ECOSSolver()))
